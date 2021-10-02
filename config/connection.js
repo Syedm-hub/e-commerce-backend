e98e2761580f42ad3b1a4a23a9207875094f639f
Syed <82619273+Syedm-hub@users.noreply.github.com>
2021-10-01 18:01:23 -0700

    pushed the wrong folder,it's corrected now

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-<<<<<<< HEAD
 let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -16,19 +15,4 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
-=======
-
-const Sequelize = require("sequelize");
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: "localhost",
-      dialect: "mysql",
-      dialectOptions: {
-        decimanNumbers: true,
-      },
-    });
-
->>>>>>> 6313e3bb51efc2f1b5f1a7213681e372cdb61431
 module.exports = sequelize;
--- models/Category.js
+++ models/Category.js
@@ -1,14 +1,9 @@
 const { Model, DataTypes } = require("sequelize");
-<<<<<<< HEAD
-const sequelize = require("../config/connection.js");
-class Category extends Model {}
-=======
 
 const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
->>>>>>> 6313e3bb51efc2f1b5f1a7213681e372cdb61431
 Category.init(
   {
     id: {
